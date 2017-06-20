47cc2f5dc1f765c07300b8430ca390918036d7e6
leebo <me@bobo.im>
2017-06-20 15:26:47 +0800

    change

--- init.rb
+++ init.rb
@@ -14,8 +14,8 @@ upyun = Upyun::Rest.new('xj8-img', "bobo", "lendlove")
 # require_all 'libs'
 
 # 新建主机
-# server = JSON.parse(RestClient.post('http://138.68.241.151:4567/servers', {}).body)["server"]
-# puts server
+server = JSON.parse(RestClient.post('http://138.68.241.151:4567/servers', {}).body)["server"]
+puts server
 # # 新建钱包
 puts "===============开始新建钱包================="
 # `echo "lendlove" >> pass`
