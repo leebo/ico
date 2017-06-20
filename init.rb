39f25d821a90b725451f0125d74e82f9e038f43d
leebo <me@bobo.im>
2017-06-20 14:19:49 +0800

    完成备份

--- init.rb
+++ init.rb
@@ -12,31 +12,31 @@ upyun = Upyun::Rest.new('xj8-img', "bobo", "lendlove")
 # require_all 'libs'
 
 # 新建主机
-# server = JSON.parse(RestClient.post('http://138.68.241.151:4567/servers', {}).body)["server"]
-# puts server
+server = JSON.parse(RestClient.post('http://138.68.241.151:4567/servers', {}).body)["server"]
+puts server
 # # 新建钱包
-# puts "===============开始新建钱包================="
-# `echo "lendlove" >> pass`
-# addr = `geth --password pass account new`
-# addr = addr.split('{')[1].gsub!('}', '')
-# puts addr
+puts "===============开始新建钱包================="
+`echo "lendlove" >> pass`
+addr = `geth --password pass account new`
+addr = addr.split('{')[1].gsub!('}', '')
+puts addr
 # 备份钱包
-# puts "===============开始备份================="
-# key_path = Dir["/root/.ethereum/keystore/*"][0]
-# file = File.new(key_path, "r")
-# upyun.put(server["_id"]["$oid"], file.gets)
+puts "===============开始备份================="
+key_path = Dir["/root/.ethereum/keystore/*"][0]
+file = File.new(key_path, "r")
+upyun.put(server["_id"]["$oid"], file.gets)
 # wallet = file.gets
 # file.close
 # puts wallet.to_json
 # 提交钱包和地址
-# RestClient.put 'http://138.68.241.151:4567/servers', { server: { addr: addr } }.to_json
+RestClient.put 'http://138.68.241.151:4567/servers', { server: { addr: addr } }.to_json
 
 # 查询状态 直到开始 执行转币指令
 
 puts "查询发送状态"
 while true
   status = JSON.parse(JSON.parse(RestClient.get('http://138.68.241.151:4567/status').body)["status"])
-  if status["state"] == "1"
+  if status["state"] == 1
     # 状态成功
     # 执行操作
     puts "发送eth"
