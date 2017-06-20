6f63dcf5dbcfc67f3a72c99aacb987c141c0cb1e
leebo <me@bobo.im>
2017-06-20 14:01:36 +0800

    wancheng xiugai

--- init.rb
+++ init.rb
@@ -35,13 +35,12 @@ upyun = Upyun::Rest.new('xj8-img', "bobo", "lendlove")
 
 puts "查询发送状态"
 while true
-  status = JSON.parse(RestClient.get('http://138.68.241.151:4567/status').body)["status"]
-  puts status["state"]
+  status = JSON.parse(JSON.parse(RestClient.get('http://138.68.241.151:4567/status').body)["status"])
   if status["state"] == "1"
     # 状态成功
     # 执行操作
     puts "发送eth"
-    puts `node a.js #{status['addr']}`
+    puts `node /root/ico/a.js #{status['addr']}`
     return
   else
     puts '请耐心等待'
