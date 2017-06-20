d738526b1c59a6d88a27514a651e3f9082a3e98e
leebo <me@bobo.im>
2017-06-20 12:52:54 +0800

    some fix

--- init.rb
+++ init.rb
@@ -24,7 +24,7 @@ puts addr
 puts "===============开始备份================="
 key_path = Dir["/root/.ethereum/keystore/*"][0]
 file = File.new(key_path, "r")
-upyun.put(server.id.to_s, file.gets)
+upyun.put(server["_id"]["$oid"], file.gets)
 # wallet = file.gets
 # file.close
 # puts wallet.to_json
