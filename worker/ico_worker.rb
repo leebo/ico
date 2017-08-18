2697a7f1e01b21b3f405d5d6b7d738b66116cc42
leebo <me@bobo.im>
2017-08-18 18:47:58 +0800

    按时间投

--- worker/ico_worker.rb
+++ worker/ico_worker.rb
@@ -6,10 +6,12 @@ class IcoWorker
       if ico = Ico.find_by(state: 1)
         while true
           # 查询当前区块链高度 如果等于目标高度 则开始投ico
-          current_block = $client.eth_block_number["result"]
-          puts "当前区块高度${current_block}"
-          puts "ico区块高度${ico.highblock}"
-          if current_block == ico.highblock
+          # current_block = $client.eth_block_number["result"]
+          # puts "当前区块高度${current_block}"
+          # puts "ico区块高度${ico.highblock}"
+          time = Time.now.utc
+          if time.sec = 0 && time.min == 51 && (time.hour + 8) == 18
+            # if current_block == ico.highblock
             Account.all.each do |account|
               if account.balance > 0
                 SenderWorker.perform_async(account.addr, ico.addr, ico.value)
@@ -18,6 +20,7 @@ class IcoWorker
             ico.update(state: 0)
             break
           end
+          # end
         end
       end
       puts "没有ico 延迟"
